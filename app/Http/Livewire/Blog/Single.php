a84eb57236b8fadb648ae290657e558a79541d5b
arsnavneet <amit.khare588@gmail.com>
2021-09-18 23:51:08 +0530

    `commit`

--- app/Http/Livewire/Blog/Single.php
+++ app/Http/Livewire/Blog/Single.php
@@ -3,11 +3,30 @@
 namespace App\Http\Livewire\Blog;
 
 use Livewire\Component;
+use App\Models\Blog;
+use App\Models\Comments;
 
 class Single extends Component
 {
-    public function render()
-    {
+    public $url, $title, $blogId;
+    public $comments = [];
+    public $response = [];
+    public $data;
+    
+    public function mount($url){
+        $this->url =        $url;
+        $this->data =       Blog::where('url', $url)->first();
+        if($this->data != null){
+            $this->title =      $this->data->title;
+            $this->blogId =      $this->data->id;
+            $this->comments =   Comments::where('blogId', $this->data->id)->where('c_order', 0)->get();
+            $this->response =   Comments::where('blogId', $this->data->id)->where('c_order', 1)->get();
+        }else{
+            return redirect('/404');
+        }
+    }
+    
+    public function render(){
         return view('livewire.blog.single');
     }
 }
--- app/Http/Livewire/Parts/Singlesidebar.php
+++ app/Http/Livewire/Parts/Singlesidebar.php
@@ -3,11 +3,16 @@
 namespace App\Http\Livewire\Parts;
 
 use Livewire\Component;
+use App\Models\Blogmeta;
 
 class Singlesidebar extends Component
 {
-    public function render()
-    {
+    public $category = [];
+    public $tag = [];
+    
+    public function render(){
+        $this->category = Blogmeta::select('id', 'name', 'url')->where('type', 'category')->get();
+        $this->tag = Blogmeta::select('id', 'name', 'url')->where('type', 'tag')->get();
         return view('livewire.parts.singlesidebar');
     }
 }
