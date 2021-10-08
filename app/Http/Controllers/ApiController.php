73f1bbb144d81d44d7a6cf68a1e892f970188623
arsnavneet <amit.khare588@gmail.com>
2021-10-08 14:09:13 +0530

    `commit`

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use DB;
 use Auth;
 use Hash;
+use App\Models\Master;
 use App\Models\Course;
 use App\Models\Product;
+use App\Models\Coursereview;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
@@ -29,6 +31,97 @@ class ApiController extends Controller
             'data' => $data
         ]);
     }
+
+    public function courseList(){
+        $data = Course::select('id', 'name', 'image', 'shortdesc', 'price', 'sale')->where('status', 1)->get();
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+
+    public function singleCourse($id){
+        $reviews =  Coursereview::leftJoin('users as b', function($join) { $join->on("b.id", "=", "coursereviews.userid"); })
+                        ->where("coursereviews.status", 1)
+                        ->where("coursereviews.courseid", $id)
+                        ->where("coursereviews.type", 'Course')
+                        ->select('coursereviews.id', 'coursereviews.review', 'coursereviews.rating', 'b.name as userName' )
+                        ->get();
+        $data = Course::select('id', 'name', 'image', 'shortdesc', 'longdesc', 'price', 'sale', 'rating', 'videos' )
+                ->where('id', $id)->first();
+
+        return response()->json([
+            'data' => $data,
+            'reviews' => $reviews
+        ]);
+    }
+
+    public function postReview(request $request){
+        $dB                     =   new Coursereview;
+        $dB->type               =   $request->type;
+        $dB->courseid           =   $request->courseid;
+        $dB->userid             =   $request->userid;
+        $dB->review             =   $request->review;
+        $dB->rating             =   $request->rating;
+        $dB->status             =   0;
+        $dB-> save();
+
+        $response = ['success'=>true, 'message' => "Review submitted for approval"];
+        return response()->json($response, 201);
+    }
+
+    public function ecomCategories(){
+        $data = Master::select('id', 'name', 'tab1')->where('type', 'prodCat')->get();
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+
+    public function ecomRecom(){
+        $id = Master::select('id')->where('type', 'prodTag')->where('name', 'Recommended')->first();
+        $data = Product::select('id', 'name', 'images', 'price', 'sale')->whereJsonContains('tag', $id->id)->get();
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+
+    public function ecomTrending(){
+        $id = Master::select('id')->where('type', 'prodTag')->where('name', 'Trending')->first();
+        $data = Product::select('id', 'name', 'images', 'price', 'sale')->whereJsonContains('tag', $id->id)->get();
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+
+    public function ecomCategoryProducts($id){
+        $data = Product::select('id', 'name', 'images', 'price', 'sale')->whereJsonContains('category', (int)$id)->get();
+        return response()->json([
+            'data' => $data
+        ]);
+    }
+
+    public function singleProduct($id){
+        $reviews =  Coursereview::leftJoin('users as b', function($join) { $join->on("b.id", "=", "coursereviews.userid"); })
+                        ->where("coursereviews.status", 1)
+                        ->where("coursereviews.courseid", $id)
+                        ->where("coursereviews.type", 'Product')
+                        ->select('coursereviews.id', 'coursereviews.review', 'coursereviews.rating', 'b.name as userName' )
+                        ->get();
+
+        $data = Product::select('id', 'name', 'images', 'shortdesc', 'longdesc', 'price', 'sale', 'rating' )
+                ->where('id', $id)->first();
+        return response()->json([
+            'data'          => $data,
+            'reviews'       => $reviews
+        ]);
+    }
+
+    public function allProducts(){
+        $data = Product::select('id', 'name', 'images', 'shortdesc', 'price', 'sale', 'rating' )->where('status', 1)->get();
+        return response()->json([
+            'data'          => $data
+        ]);
+    }
+
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -11,5 +11,17 @@ Route::post('/login', 'App\Http\Controllers\AuthController@login');
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/products', 'App\Http\Controllers\ApiController@products');
     Route::get('/featuredCourses', 'App\Http\Controllers\ApiController@featuredCourses');
+    Route::get('/courseList', 'App\Http\Controllers\ApiController@courseList');
+    Route::get('/singleCourse/{id}', 'App\Http\Controllers\ApiController@singleCourse');
+    Route::post('/postReview', 'App\Http\Controllers\ApiController@postReview');
+    
+    Route::get('/ecomCategories', 'App\Http\Controllers\ApiController@ecomCategories');
+    Route::get('/ecomRecom', 'App\Http\Controllers\ApiController@ecomRecom');
+    Route::get('/ecomTrending', 'App\Http\Controllers\ApiController@ecomTrending');
+    Route::get('/ecomCategoryProducts/{id}', 'App\Http\Controllers\ApiController@ecomCategoryProducts');
+    Route::get('/singleProduct/{id}', 'App\Http\Controllers\ApiController@singleProduct');
+    Route::get('/allProducts', 'App\Http\Controllers\ApiController@allProducts'); // has to be Created
+
+    Route::get('/applyCode', 'App\Http\Controllers\ApiController@applyCode'); // has to be Created
 });
 
