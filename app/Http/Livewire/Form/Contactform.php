db284572a898d2bd1646916f2ff8aff8eb52b61a
arsnavneet <amit.khare588@gmail.com>
2021-10-29 01:27:27 +0530

    `commit`

--- app/Http/Livewire/Form/Contactform.php
+++ app/Http/Livewire/Form/Contactform.php
@@ -3,11 +3,50 @@
 namespace App\Http\Livewire\Form;
 
 use Livewire\Component;
+use App\Models\Contact;
 
 class Contactform extends Component
 {
-    public function render()
-    {
+    public $name, $email, $phone, $message;
+    public $url = '';
+
+    public function mount(){
+        $url = str_replace("http://asifweb.test","", url()->current());
+        $this->url = $url;
+    }
+
+    public function render(){
         return view('livewire.form.contactform');
+    }
+
+    public function submit(){
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'message' => 'required',
+        ]);
+
+        Contact::create([
+            'url' =>  $this->url,
+            'name' =>  $this->name,
+            'email' =>  $this->email,
+            'phone' =>  $this->phone,
+            'message' =>  $this->message,
+        ]);
+
+        // $xx = [
+        //     'name' =>  $this->name,
+        //     'email' =>  $this->email,
+        //     'phone' =>  $this->phone,
+        //     'city' =>  $this->city,
+        //     'reason' =>  $this->reason,
+        // ];
+
+        // $user_email = $this->email;
+        // Mail::to( $user_email)->cc('amit@amitkk.com')->send(new JoinMail($xx));
+
+        session()->flash('message', 'Form Submitted Successfully.');
+        return redirect(route('thankyou') );
     }
 }
